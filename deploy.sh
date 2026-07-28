a9348bd38ac1fcc050a3b254c25fad7956eef94d
Vinay S <vinays9751@gmail.com>
2026-07-28 21:47:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker login --username AWS --password-stdin 278396816876.dkr.ecr.us-east-2.amaz
 
 echo "Stopping existing container..."
 
-docker stop my-app-web-container || true 
+docker stop my-app-web-container || true
 
 echo "Removing existing container..."
 
@@ -24,8 +24,8 @@ docker pull 278396816876.dkr.ecr.us-east-2.amazonaws.com/my-app-web:latest
 echo "Starting new container..."
 
 docker run -d \
---name my-app-web-container \
--p 80:5000 \
-278396816876.dkr.ecr.us-east-2.amazonaws.com/my-app-web:latest
+  --name my-app-web-container \
+  -p 80:5000 \
+  278396816876.dkr.ecr.us-east-2.amazonaws.com/my-app-web:latest
 
 echo "Deployment completed successfully."
